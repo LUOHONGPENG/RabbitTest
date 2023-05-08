dad112526e66c1726a43f2243323d1405cf81b9d
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-08 21:15:41 +0100

    RabbitTestExcelFile

---
+++ Assets/Scripts/ExcelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+ 
+public class ExcelManager : Singleton<ExcelManager>
+{
+    Dictionary<Type, object> excelDataDic = new Dictionary<Type, object>();
+ 
+    public T GetExcelData<T, V>() where T : ExcelDataBase<V> where V : ExcelItemBase
+    {
+        Type type = typeof(T);
+        if (excelDataDic.ContainsKey(type) && excelDataDic[type] is T)
+            return excelDataDic[type] as T;
+ 
+        T excelData = Resources.Load<T>("ExcelAsset/"+ type.Name);
+ 
+        if (excelData != null)
+            excelDataDic.Add(type, excelData as T);
+ 
+        return excelData;
+    }
+ 
+    public V GetExcelItem<T, V>(string targetId) where T : ExcelDataBase<V> where V : ExcelItemBase
+    {
+        var excelData = GetExcelData<T, V>();
+ 
+        if (excelData != null)
+            return excelData.GetExcelItem(targetId);
+        return null;
+    }
+ 
+}
